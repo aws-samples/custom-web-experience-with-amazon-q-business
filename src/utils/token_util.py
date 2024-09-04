d2700fcc6e7d417a3a6d2c207719800d8ad3e99c
Ahmed Mohamed <ahmoe@amazon.com>
2024-09-04 12:00:18 -0400

    next try

--- src/utils/token_util.py
+++ src/utils/token_util.py
@@ -12,11 +12,11 @@ def refresh_iam_oidc_token(refresh_token, idc_application_id, region):
     )
     return response
 
-def get_iam_oidc_token(id_token, idc_application_id, region):
+def get_iam_oidc_token(id_token, idc_application_id):
     """
     Retrieve the IAM OIDC token using the ID token.
     """
-    client = boto3.client("sso-oidc", region_name=region)
+    client = boto3.client("sso-oidc", region_name="us-east-1")
     response = client.create_token(
         clientId=idc_application_id,
         grantType="urn:ietf:params:oauth:grant-type:jwt-bearer",
