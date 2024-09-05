214dfca969816dfc4ff8b1342abfc9f164f4b021
Ahmed Mohamed <ahmoe@amazon.com>
2024-09-05 11:28:45 -0400

    minor fix

--- src/utils/translation_util.py
+++ src/utils/translation_util.py
@@ -2,7 +2,7 @@ import boto3
 import streamlit as st
 
 def translate_text(text, source_language_code='auto', target_language_code='en'):
-    translate = boto3.client('translate', region_name=st.session_state.REGION)
+    translate = boto3.client('translate', region_name="us-east-1")
     result = translate.translate_text(
         Text=text,
         SourceLanguageCode=source_language_code,
